482ce038a99cca349570a232b0da609427a32ae3
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2023-10-25 15:30:12 -0500

    Fin Seccion 16

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+import { canActivatePublicGuard, canMatchPublicGuard } from './auth/guards/public.guard';
+import { canActivateGuard, canMatchGuard } from './auth/guards/auth.guard';
+
+
 
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m=> m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then( m=> m.AuthModule),
+    canActivate: [canActivatePublicGuard], //Anclamos la función del canActive
+    canMatch: [canMatchPublicGuard], //Anclamos la función del canMatch
   },
   {
     path: 'heroes',
-    loadChildren: () => import('./heroes/heroes.module').then( m=> m.HeroesModule)
+    loadChildren: () => import('./heroes/heroes.module').then( m=> m.HeroesModule),
+    canActivate: [canActivateGuard], //Anclamos la función del canActive
+    canMatch: [canMatchGuard], //Anclamos la función del canMatch
   },
   {
     path: '404',
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
+import { User } from './../../../auth/interfaces/user.interface';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-layout-page',
@@ -8,11 +11,27 @@ import { Component } from '@angular/core';
 })
 export class LayoutPageComponent {
 
+
+
+
   public sidebarItems = [
     {label: 'listado', icon: 'label', url: './list'},
     {label: 'Añadir', icon: 'add', url: './new-hero'},
     {label: 'buscar', icon: 'search', url: './search'}
 
   ]
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ){}
+
+  get user(): User | undefined {
+    return this.authService.currentUser
+  }
+
+  onLogout(){
+    this.authService.logout()
+    this.router.navigate(['/auth/login'])
+  }
 
 }
